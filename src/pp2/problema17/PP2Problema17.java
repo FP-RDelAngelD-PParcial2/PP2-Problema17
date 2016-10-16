5e9c9ecfbaab00bdf4e272bd4dc9a7dc26d46d13
PC <PC@192.168.1.70>
2016-10-16 18:16:54 -0500

    17.	Realiza un programa que contenga una función que calcule el máximo común divisor de dos números.

--- src/pp2/problema17/PP2Problema17.java
+++ src/pp2/problema17/PP2Problema17.java
@@ -38,21 +38,22 @@ public class PP2Problema17 {
         int numeromenor, c, divisor; //Declaracion de variables
         c=1; //Debe ser 1 ya que si es 0 se elimina el divisor
         divisor=2; //Comienza en 2 ya que es lo minimo en lo que se pueden dividir (media)
-        if(a>b){ //Se necesita conocer el numero menor para saber en cuanto se puede dividir
-            numeromenor=b; //Si a es mayor que b, b es el numero menor
+        if(a>b){ //Se necesita conocer el numero menor para saber en cuanto se puede dividir //2>8
+            numeromenor=b; //Si a es mayor que b, b es el numero menor //FALSO
         }else{
-            numeromenor=a; //Sino el numero menor es a
+            numeromenor=a; //Sino el numero menor es a //numeromenor=8
         }
-        while(numeromenor>=divisor){ //Mientras que el numero menor sea mayor o igual al divisor,
-            if(a%divisor==0 && b%divisor==0){ //Si el residuo de la division de a entre el divisor es 0
-                                              //Si el residuo de la division de b entre el divisor es 0
-                a=a/divisor; //a
-                b=b/divisor;
-                c=c*divisor;
-            }else{
+        while(numeromenor>=divisor){ //Mientras que el numero menor sea mayor o igual al divisor, //8>=2 VERDADERO
+            if(a%divisor==0 && b%divisor==0){ //Si el residuo de la division de a entre el divisor es 0 //2%2==0 && 8%2==0
+                                              //y si el residuo de la division de b entre el divisor es 0 //VERDADERO && VERDADERO = VERDADERO
+                a=a/divisor; //a sera igual a la division de a entre el divisor //a=2/2 a=1
+                b=b/divisor;//b sera igual a la division de a entre el divisor  //b=8/2 b=4
+                c=c*divisor;//c sera igual a la multiplicacion de c por le divisor //c=1*2 c=2
+            }else{ //Sino se cumple el if a divisor se le sumara 1
                 divisor++;
             }
         }
+        //Se imprime el maximo comun denominador //c=2
         System.out.print("El maximo comun denominador es: " + c);
     }
 }
